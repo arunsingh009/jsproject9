3f615fb8580f2f28fb5a1ae9b19ce96944c95cbb
Arun Kumar Singh <66819980+arunsingh009@users.noreply.github.com>
2021-12-26 01:07:14 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var images = ['./images/pic1.jpg', './images/pic2.jpg', './images/pic3.jpg', './images/pic4.jpg', './images/pic5.jpg', './images/pic6.jpg'];
+var imgIndex = 0;
+var imgDuration = 3000;
+
+
+function forward() {
+
+    document.getElementById('slider').className += "fadeOut";
+    setTimeout(function () {
+        document.getElementById('slider').src = images[imgIndex];
+        document.getElementById('slider').className = "";
+    }, 1000);
+
+    imgIndex = imgIndex + 1;
+    if (imgIndex == (images.length) - 1) {
+        imgIndex = 0
+    }
+    document.getElementById("slider").src = images[imgIndex];
+    setTimeout(forward, imgDuration);
+
+}
+forward();
